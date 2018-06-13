4a9426c1f75694d949fa7a8de6087327a91f137f
SongTranVan <songtranvan2511@gmail.com>
2018-06-13 08:56:16 +0700

    finally finish chapter 13

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   include SessionsHelper
+
+  private
+
+  # Confirms a logged-in users
+  def logged_in_user
+    return if logged_in?
+    store_location
+    flash[:danger] = t "users.login_msg"
+    redirect_to login_url
+  end
 end
